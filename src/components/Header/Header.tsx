57a9da9f9c0d290331be5507bcc4cf970cdb8e74
Denis Kalkopf <ddenproton@protonmail.com>
2025-03-12 14:46:42 +0500

    fix: remove console errors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import {
   Sheet,
   SheetContent,
   SheetDescription,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
@@ -15,33 +16,35 @@ export default function Header() {
             <Menu />
           </SheetTrigger>
           <SheetContent side="left" className="w-full sm:w-[540px]">
-            <SheetDescription className="px-[20px] h-full flex flex-col justify-center">
-              <nav>
-                <ul className="font-bold text-[22px] text-neutral-950 uppercase flex flex-col gap-[28px]">
-                  <li>
-                    <a href="/">Science</a>
-                  </li>
-                  <li>
-                    <a href="/">General</a>
-                  </li>
-                  <li>
-                    <a href="/">Entertainment</a>
-                  </li>
-                  <li>
-                    <a href="/">Technology</a>
-                  </li>
-                  <li>
-                    <a href="/">Business</a>
-                  </li>
-                  <li>
-                    <a href="/">Health</a>
-                  </li>
-                  <li>
-                    <a href="/">Sports</a>
-                  </li>
-                </ul>
-              </nav>
+            <SheetTitle className="sr-only">Menu</SheetTitle>
+            <SheetDescription className="sr-only">
+              Main nav menu
             </SheetDescription>
+            <nav className="px-[20px] h-full flex flex-col justify-center">
+              <ul className="font-bold text-[22px] text-neutral-950 uppercase flex flex-col gap-[28px]">
+                <li>
+                  <a href="/">Science</a>
+                </li>
+                <li>
+                  <a href="/">General</a>
+                </li>
+                <li>
+                  <a href="/">Entertainment</a>
+                </li>
+                <li>
+                  <a href="/">Technology</a>
+                </li>
+                <li>
+                  <a href="/">Business</a>
+                </li>
+                <li>
+                  <a href="/">Health</a>
+                </li>
+                <li>
+                  <a href="/">Sports</a>
+                </li>
+              </ul>
+            </nav>
           </SheetContent>
         </Sheet>
         <p className="font-bold text-2xl uppercase">Besider</p>
